e435f18d891e469ecad83074de1da27b23ad3a08
arramsrija <136781241+arramsrija@users.noreply.github.com>
2023-08-24 15:16:42 +0530

    Create app.module.ts

---
+++ bootstrap/Form Validations/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { ReactiveFormsModule} from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+

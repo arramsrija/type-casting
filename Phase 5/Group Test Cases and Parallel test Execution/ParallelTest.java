3f83bd34ea75397aae0e193fa65762588dd499f4
arramsrija <136781241+arramsrija@users.noreply.github.com>
2023-09-01 15:14:39 +0530

    Create  ParallelTest.java

---
+++ Phase 5/Group Test Cases and Parallel test Execution/ParallelTest.java
@@ -0,0 +1,84 @@
+package com.ecommerce.test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+@SuppressWarnings("unused")
+publicclass ParallelTest {
+public String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
+private WebDriver driver;
+@Test(groups = {"Account Creation"})
+publicvoid facebookAccCreation() {
+System.out.println("Inside facebookAccCreation");
+System.out.println("Thread ID " +Thread.currentThread().getId());
+String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
+driver.get(baseUrl);
+String cssDay = "#day";
+WebElement cssDaySelect = driver.findElement(By.cssSelector(cssDay));
+//softassert.assertNotNull(cssDaySelect);
+Select daySelect = new Select(cssDaySelect);
+daySelect.selectByVisibleText("11");
+WebElement cssGenderRadio = driver.findElement(By.cssSelector("span > span > input[type='radio'][value='2']"));
+cssGenderRadio.click();
+System.out.println("Gender is enabled = " + cssGenderRadio.isSelected());
+}
+@Test(groups = "Account Creation", dependsOnGroups = { "Launch" }, 
+dataProvider = "googleUserData")
+publicvoid googleAccCreation(String fName, String lName) {
+System.out.println("Inside googleAccCreation");
+System.out.println("Thread ID " +Thread.currentThread().getId());
+String baseUrl = "https://accounts.google.com/signup/v2?biz=true&flowEntry=SignUp";
+driver.get(baseUrl);
+// Let's locate the first name text field by its id.
+WebElement firstNameTF = driver.findElement(By.id("firstName"));
+firstNameTF.sendKeys(fName) ;//("Myname");
+// Let's locate the surname name text field by its name.
+WebElement surNameTF = driver.findElement(By.name("lastName"));
+surNameTF.sendKeys(lName) ;//("latName");
+// The Next button using its class
+WebElement nextButton = driver.findElement(By.className("VfPpkd-vQzf8d"));
+System.out.println("Text on the button is " + nextButton.getText());
+}
+@Test(groups = "Launch")
+publicvoid googleWebsiteTitle() {
+System.out.println("Inside googleWebsiteTitle");
+System.out.println("Thread ID " +Thread.currentThread().getId());
+String baseUrl = "https://www.google.com";
+driver.get(baseUrl);
+System.out.println("Google Title is " + driver.getTitle());
+}
+@Test
+publicvoid f1() {
+System.out.println("Inside f1 in Thread ID " +Thread.currentThread().getId());
+}
+@Test
+publicvoid f2() {
+System.out.println("Inside f2 in Thread ID " +Thread.currentThread().getId());
+}
+@DataProvider(name = "googleUserData")
+public Object[][] createData1() {
+returnnew Object[][] {
+{"fname1","lname1"},
+{"fname2","lname2"}
+};
+}
+@BeforeClass
+publicvoid beforeClass() {
+driver = new ChromeDriver();
+}
+@AfterClass
+publicvoid afterClass() {
+driver.quit();
+}
+}
